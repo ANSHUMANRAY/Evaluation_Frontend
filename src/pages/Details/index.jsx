7914773161e0633c713a590d6dcc8234014f148f
Anshuman Ray <rayanshuman98@gmail.com>
2023-03-01 17:03:06 +0530

    feat: add Home and detail pages

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header, EventDetails, Footer} from '../../components';
+
+export default function Details() {
+  return (
+    <div>
+      <Header />
+      <EventDetails />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header, Body, Footer} from '../../components';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
